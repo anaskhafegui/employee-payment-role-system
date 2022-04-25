2958e25000dabb4dc2eccc9209a2a0d597aad3ca
Anas Khafegui <anas.mdhat@outlook.com>
2022-04-25 21:14:51 +0200

    add employees and payments tables

---
+++ database/migrations/2022_04_25_173536_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->date('payment_date')->unique()->useCurrent();     
+            $table->decimal('payment', $precision = 8, $scale = 2);
+            $table->string('payment_day');
+            $table->enum('type', ['bonus', 'salary']);
+            $table->index(['payment_date']);
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
