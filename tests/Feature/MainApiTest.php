80a127a66baf548acd54c50bef10ef2f6e62d3a8
Anas Khafegui <anas.mdhat@outlook.com>
2022-04-27 03:29:19 +0200

    add test getAllPayments endpoint

--- tests/Feature/MainApiTest.php
+++ tests/Feature/MainApiTest.php
@@ -7,6 +7,7 @@ use Tests\TestCase;
 use Laravel\Sanctum\Sanctum;
 use App\Models\User;
 use App\Models\Employee;
+use App\Classes\PaymentDate;
 
 class MainApiTest extends TestCase
 {
@@ -37,7 +38,37 @@ class MainApiTest extends TestCase
            ->assertStatus(200)
            ->assertJson(['status' => 1, 'message' => 'bouns percentages changed successfully']);
     }
+    public function test_the_admin_can_get_payments_dates_for_the_remainder_of_this_year()
+    {
+       $user  = User::factory()->create();
+       $token = $user->createToken($user->email)->plainTextToken;
+       $month = Null;
+       $remainderdates = new PaymentDate(); 
+       $remainderdates = $remainderdates->getAllPayments($month);
+       $headers = ['Authorization' => "Bearer $token"];
+       $payload = [
+           'month' => NULL,
+       ];
 
+       $this->json('GET', 'api/admin/getAllPayments', $payload, $headers)
+           ->assertStatus(200)
+           ->assertJson($remainderdates);
+    }
+    public function test_the_admin_can_get_payments_dates_for_the_remainder_of_this_year_with_filter()
+    {
+       $user  = User::factory()->create();
+       $token = $user->createToken($user->email)->plainTextToken;
+       $month = "May";
+       $remainderdates = new PaymentDate(); 
+       $remainderdates = $remainderdates->getAllPayments($month);
+       $headers = ['Authorization' => "Bearer $token"];
+       $payload = [
+           'month' => "May",
+       ];
 
+       $this->json('GET', 'api/admin/getAllPayments', $payload, $headers)
+           ->assertStatus(200)
+           ->assertJson($remainderdates);
+    }
   
 }
